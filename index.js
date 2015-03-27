fe28138373705503484a516172fada85be530fdf
Brendan Long <self@brendanlong.com>
2015-03-27 11:21:18 -0500

    Add index.js.

---
+++ index.js
@@ -0,0 +1,74 @@
+/*!
+ * express-timeout-header
+ * Copyright (c) 2015, CableLabs, Inc.
+ * All rights reserved.
+ *
+ * Written by Brendan Long <b.long@cablelabs.com>
+ * License: BSD-2-Clause (http://opensource.org/licenses/BSD-2-Clause)
+ */
+"use strict";
+
+var fs = require("fs");
+var path = require("path");
+
+var defaults = {
+    maxTimeout: 60000,
+    requireHeader: true
+};
+
+module.exports = function(root, options) {
+    for (var key in defaults) {
+        if (!(key in options)) {
+            options[key] = defaults[key];
+        }
+    }
+
+    return function(req, res, next) {
+        var file = path.join(root, "./" + req.url);
+
+        /* Don't bother watching files outside of the directory */
+        if (file.indexOf(root) !== 0) {
+            return next();
+        }
+
+        if (req.url[req.url.length - 1] == "/") {
+            file = path.join(file, "index.html");
+        }
+
+        fs.exists(file, function(exists) {
+            if (exists) {
+                return next();
+            }
+
+            var timeout = 0;
+            if ("timeout" in req.headers) {
+                timeout = Math.min(options.maxTimeout, req.headers.timeout);
+            } else if (!options.requireHeader) {
+                timeout = options.maxTimeout;
+            }
+            if (timeout <= 0) {
+                return next();
+            }
+
+            var watcher = null;
+            var timer = null;
+            var done = function() {
+                if (watcher) {
+                    watcher.close();
+                }
+                clearTimeout(timer);
+                next();
+            };
+            try {
+                watcher = fs.watch(path.dirname(file), function(event, changedFile) {
+                    if (changedFile == path.basename(file)) {
+                        done();
+                    }
+                });
+                timer = setTimeout(done, timeout);
+            } catch (err) {
+                done();
+            }
+        });
+    };
+};
